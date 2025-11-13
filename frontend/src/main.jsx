6062fabc61580c0fa6f457c7076c228cbbdbac3f
samiul-ahm <samiulahm09@gmail.com>
2025-11-13 18:32:50 +0600

    added login page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Login from './pages/Login.jsx'
+
+
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Login></Login>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+  <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
