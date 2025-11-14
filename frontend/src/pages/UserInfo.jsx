58b482c770c9df54026e324143c190e5d93840df
Ahmed Hussain <ahmedhussainnow@gmail.com>
2025-11-14 19:28:00 +0600

    FINAL STABLE PUSH

--- frontend/src/pages/UserInfo.jsx
+++ frontend/src/pages/UserInfo.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { fetchWithRefresh } from "../utils/fetchUtil.js";
+import AdminNabar from "../components/admin/AdminNavbar.jsx"
 
 const UsersInfo = () => {
   const { id } = useParams(); // get the dynamic user ID from the URL
@@ -34,16 +35,42 @@ const UsersInfo = () => {
   if (!user) return <div>User not found</div>;
 
   return (
-    <div>
-      <h1>{user.name}</h1>
-      <p>Email: {user.email}</p>
-      <p>Student ID: {user.studId}</p>
-      <p>Department: {user.dept}</p>
-      <p>Section: {user.section}</p>
-      <p>Session: {user.session}</p>
-      <p>Role: {user.role}</p>
-          
+    <>
+
+   <AdminNabar/>
+    <div className="flex justify-center items-center min-h-screen py-20 rounded-2xl">
+      <ul className="list bg-base-100 rounded-box shadow-2xl p-10">
+        <li className="p-5 pb-2 text-2xl opacity-60 tracking-wide">
+          Student Account Details
+        </li>
+
+        <li className="list-row">
+          <div className="text-4xl font-thin opacity-40 tabular-nums">01</div>
+          <div className="list-col-grow space-y-2">
+            <div className="text-lg font-semibold">{user.name}</div>
+            <div className="text-xs font-semibold opacity-60">
+              Email: {user.email}
+            </div>
+            <div className="text-xs font-semibold opacity-60">
+              Student ID: {user.studId}
+            </div>
+            <div className="text-xs font-semibold opacity-60">
+              Department: {user.dept}
+            </div>
+            <div className="text-xs font-semibold opacity-60">
+              Section: {user.section}
+            </div>
+            <div className="text-xs font-semibold opacity-60">
+              Session: {user.session}
+            </div>
+            <div className="text-xs font-semibold opacity-60">
+              Role: {user.role}
+            </div>
+          </div>
+        </li>
+      </ul>
     </div>
+     </>
   );
 };
 
